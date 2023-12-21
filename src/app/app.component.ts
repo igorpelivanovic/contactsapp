2ec7f8f7aac19fb5686caf137ff5bc24f1ed8eff
Igor Pelivanović <igor.pelivanovic@gmail.com>
2023-12-21 17:08:10 +0100

    init contacts route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContactsListComponent } from './contactsList/contacts-list/contacts-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContactsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'contacts', loadComponent: ()=>import('./contactsList/contacts-list/contacts-list.component').then((m)=>m.ContactsListComponent)}
+];
