b4b574ec43617b8d78bd2faeda419b7dcad94b5d
Igor Pelivanović <igor.pelivanovic@gmail.com>
2023-12-23 18:17:40 +0100

    wrapper design 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faWifi, faBatteryFull } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  datePipe = inject(DatePipe)
+  
   title = 'contactsapp';
+  time = this.currentTime
+
+  private icons = {
+    faWifi: faWifi,
+    faBatteryFull: faBatteryFull
+  }
+
+  get icon(){
+    return this.icons
+  }
+
+  get currentTime(): string{
+    return this.datePipe.transform( new Date() ,'HH:mm') as string
+  }
+
+  ngOnInit(): void {
+    setInterval(()=>{
+      this.time = this.currentTime 
+    }, 1000)
+  }
+
 }
