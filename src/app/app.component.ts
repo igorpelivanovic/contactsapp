ecc014539bd85eb261e7e71179ca98e69b68d4f5
Igor Pelivanović <igor.pelivanovic@gmail.com>
2023-12-26 15:34:53 +0100

    test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MobileInfoLineComponent } from './components/mobile-info-line/mobile-info-line.component';
+import { ContactsListComponent } from './contactsList/contacts-list/contacts-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MobileInfoLineComponent],
+  imports: [CommonModule, RouterOutlet, MobileInfoLineComponent, ContactsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
